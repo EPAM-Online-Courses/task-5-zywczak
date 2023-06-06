155d7020111eb0afd9b99e56e82adac6108f4692
zywczak <123404938+zywczak@users.noreply.github.com>
2023-06-06 21:26:59 +0200

    Update DataProvider.java

--- src/main/java/efs/task/collections/data/DataProvider.java
+++ src/main/java/efs/task/collections/data/DataProvider.java
@@ -31,15 +31,15 @@ public class DataProvider {
 
     //TODO Analogicznie do getTownsList utwórz listę miast na podstawie tablicy Data.DLCTownsArray
     public List<Town> getDLCTownsList() {
-        List<Town> listDlcTowns = new ArrayList<>();
-        for (String listDlcTowns : Data.dlcTownsArray) {
-            String[] townData = listDlcTowns.split(DATA_SEPARATOR);
+        ist<Town> towns = new ArrayList<>();
+        for (String town : Data.dlcTownsArray) {
+            String[] townData = town.split(DATA_SEPARATOR);
             List<String> startingHeroes = new ArrayList<>();
             startingHeroes.add(townData[1].trim());
             startingHeroes.add(townData[2].trim());
-            listDlcTowns.add(new Town(townData[0], startingHeroes));
+            towns.add(new Town(townData[0], startingHeroes));
         }
-        return listDlcTowns;
+        return towns;
     }
 
     //TODO Na podstawie tablicy Data.baseCharactersArray utworzyć listę bohaterów dostępnych w grze.
@@ -47,21 +47,21 @@ public class DataProvider {
     // Korzystając z funkcji split() oraz DATA_SEPARATOR utwórz listę unikalnych obiektów efs.task.collections.entities.Hero.
     // UWAGA w Data.baseCharactersArray niektórzy bohaterowie powtarzają się, do porównania bohaterów używamy zarówno imie jak i jego klasę;
     public Set<Hero> getHeroesSet() {
-        Set<Hero> heroesSet = new HashSet<>();
+        Set<Hero> heroes = new HashSet<>();
         for (String hero : Data.baseCharactersArray) {
             String[] heroData = hero.split(DATA_SEPARATOR);
-            heroesSet.add(new Hero(heroData[0].trim(), heroData[1].trim()));
+            heroes.add(new Hero(heroData[0].trim(), heroData[1].trim()));
         }
-        return heroesSet;
+        return heroes;
     }
 
     //TODO Analogicznie do getHeroesSet utwórz listę bohaterów na podstawie tablicy Data.DLCCharactersArray
     public Set<Hero> getDLCHeroesSet() {
-        Set<Hero> heroesDlcSet = new HashSet<>();
+        Set<Hero> heroes = new HashSet<>();
         for (String hero : Data.dlcCharactersArray) {
             String[] heroData = hero.split(DATA_SEPARATOR);
-           heroesDlcSet.add(new Hero(heroData[0].trim(), heroData[1].trim()));
+            heroes.add(new Hero(heroData[0].trim(), heroData[1].trim()));
         }
-        return heroesDlcSet;
+        return heroes;
     }
 }
